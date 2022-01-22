98ff9592d7b3acb7016c1872d0cf473cfad0b660
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-21 19:48:19 -0600

    Refactoring signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+// To be replaced:
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
